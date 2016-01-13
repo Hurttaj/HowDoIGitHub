53482a44d7bc5d678249e15643c7a16ec7046571
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-13 14:17:07 +0200

    Osataan if lauseita.

--- Suurin/Suurin/Program.cs
+++ Suurin/Suurin/Program.cs
@@ -10,6 +10,24 @@ namespace Suurin
     {
         static void Main(string[] args)
         {
+            Console.Write("Anna Luku: ");
+            int Luku1 = int.Parse(Console.ReadLine());
+            Console.Write("Anna Luku: ");
+            int Luku2 = int.Parse(Console.ReadLine());
+            Console.Write("Anna Luku: ");
+            int Luku3 = int.Parse(Console.ReadLine());
+            if (Luku1 > Luku2 && Luku1 > Luku3)
+            {
+                Console.WriteLine("Suurin luku on " + Luku1);
+            }
+            else if (Luku2 > Luku1 && Luku2 > Luku3)
+            {
+                Console.WriteLine("Suurin luku on " + Luku2);
+            }
+            else if (Luku3 > Luku1 && Luku3 > Luku1)
+            {
+                Console.WriteLine("Suurin luku on " + Luku3);
+            }
         }
     }
 }
