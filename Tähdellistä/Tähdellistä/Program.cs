4efe51bf7de41138d758a0579f67784cd54685fb
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-13 15:06:09 +0200

    for loops

---
+++ Tähdellistä/Tähdellistä/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tähdellistä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna Luku: ");
+            int X = int.Parse(Console.ReadLine());
+            for(int Y=0;Y<X;Y++) // Y on laskuri joka kertoo loopille montako riviä looppi tulostaa. Rivejä on niin monta kuin käyttäjältä pyydetyssä X-arvossa sanotaan.
+            {
+                for(int i=-1;i<Y;i++) // i kertoo montako * merkkiä yhdelle riville tulee. i pyörii kunnes se on yhtä suuri kuin Y, eli * -merkkien määrä on sama kuin se monennessako loopin iteraatiossa ollaan.
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
