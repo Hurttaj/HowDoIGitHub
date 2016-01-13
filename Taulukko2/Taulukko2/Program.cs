768679accbb0f23f0e567798803c8f909f23c21b
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-13 15:14:26 +0200

    Velmuja taulukkoja.

---
+++ Taulukko2/Taulukko2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taulukko2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Taulu = new int[5];
+            int x = 0;
+            while (x<5)
+            {
+                Console.Write("Anna luku: ");
+                Taulu[x] = int.Parse(Console.ReadLine());
+                x++;
+            }
+            while (x>0)
+            {
+                Console.Write(Taulu[x-1] + ", ");
+                x--;
+            }
+        }
+    }
+}
