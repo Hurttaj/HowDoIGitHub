772c8304f3e84496759fd48f42f99205c487541f
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-19 10:22:00 +0200

    jutskuja

---
+++ Demo3/Demo3/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Speed { get; set; }
+        public double Engine { get; set; }
+        public bool FuzzyDice { get; set; }
+
+        // constructor
+        public Car()
+        {
+            FuzzyDice = true;
+        }
+
+        // parametric constructor
+        public Car(string model)
+        {
+            Model = model;
+        }
+
+        public void PrintData() // method displays car data
+        {
+            Console.WriteLine("Car data:");
+            Console.WriteLine("- Model: " + Model);
+            Console.WriteLine("- Color: " + Color);
+            Console.WriteLine("- Engine: " + Engine);
+            Console.WriteLine("- Speed: " + Speed);
+            Console.WriteLine("- FuzzyDice: " + FuzzyDice);
+
+        }
+
+        // method gives more speed
+        public void Accelerate(int newSpeed)
+        {
+            Speed += newSpeed;
+        }
+    }
+}
--- Demo3/Demo3/Program.cs
+++ Demo3/Demo3/Program.cs
@@ -10,6 +10,26 @@ namespace Demo3
     {
         static void Main(string[] args)
         {
+            Car trabant = new Car(); // create Car object
+            trabant.Model = "Trabant";
+            trabant.Color = "Musta";
+            trabant.Speed = 30;
+            trabant.Engine = 0.5;
+            trabant.FuzzyDice = false;
+
+            Car nascar = new Car("Toyota");
+            nascar.Speed = 200;
+            nascar.Color = "Sininen";
+            nascar.Engine = 9001.5;
+
+            trabant.Accelerate(5);
+            nascar.PrintData();
+            trabant.PrintData();
+            
+
+            Console.ReadLine();
+        
+            
         }
     }
 }
