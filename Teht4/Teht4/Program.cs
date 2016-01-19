a809fb7732e8bf47a4fe48f6aa707884b69ec179
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-19 11:08:30 +0200

    Tehtävä 4 jutskuja

---
+++ Teht4/Teht4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle truck = new Vehicle();
+            truck.Name = "Sisu";
+            truck.Speed = 50;
+            truck.Tyres = 30;
+
+            truck.PrintData();
+            Console.ReadLine();
+            truck.Speed = 60;
+            Console.WriteLine(truck.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Teht4/Teht4/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht4
+{
+    class Vehicle
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Vehicle data:");
+            Console.WriteLine("Speed: " + Speed);
+            Console.WriteLine("Tyres: " + Tyres);
+        }
+
+        public override string ToString()
+        {
+            return "Name : " + Name + "Speed : " + Speed + "Tyres: " + Tyres;
+        }
+    }
+}
