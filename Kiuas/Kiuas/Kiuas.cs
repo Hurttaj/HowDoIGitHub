f6b8cb6a7ffe66206044223ebcc81018d74011d7
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-19 10:55:19 +0200

    Osaan ohjelmoida kiukaan jeeeeeeeeeeeeeeeeeee

---
+++ Kiuas/Kiuas/Kiuas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiuas
+{
+    class Kiuas
+    {
+        public bool IsOn { get; set; }
+        public int Temperature { get; set; }
+        public int Humidity { get; set; }
+
+        public void OnOff()
+        {
+            if (IsOn == true)
+                {
+                IsOn = false;
+                }
+            else
+                {
+                IsOn = true;
+                }
+        }
+
+        public void IncreaseTemp()
+        {
+            Temperature += 5;
+        }
+
+        public void DecreaseTemp()
+        {
+            Temperature -= 5;
+        }
+
+        public void IncreaseHumid()
+        {
+            Humidity += 5;
+        }
+
+        public void DecreaseHumid()
+        {
+            Humidity -= 5;
+        }
+
+
+        public void PrintData()
+        {
+            Console.WriteLine("Heater data:");
+            Console.WriteLine("Is on: " + IsOn);
+            Console.WriteLine("Temperature: {0} Humidity {1}", Temperature, Humidity);
+        }
+        public override string ToString()
+        {
+            return "Temperature : " + Temperature + "Humidity : " + Humidity;
+        }
+    }
+
+}
---
+++ Kiuas/Kiuas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiuas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kiuas heater = new Kiuas();
+            heater.IsOn = true;
+            heater.Temperature = 100;
+            heater.Humidity = 95;
+            heater.PrintData();
+
+            Console.ReadLine();
+
+            heater.DecreaseHumid();
+            heater.DecreaseTemp();
+            heater.OnOff();
+            heater.PrintData();
+
+            Console.ReadLine();
+        }
+    }
+}
