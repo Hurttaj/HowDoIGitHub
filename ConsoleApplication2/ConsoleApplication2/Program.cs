9a754bd99f4d84dc65231eb1d37e5660f2c3daaa
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-20 11:19:23 +0200

    Onpahan tehtävä.

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book Kirja = new Book();
+            Kirja.PageCount = 200;
+            Kirja.Name = "Mars";
+            Kirja.Author = "Ben Bova";
+            Kirja.Genre = "Sci-Fi";
+
+            Kirja.PrintData();
+            Kirja.DecreaseCount();
+            Kirja.NameChange();
+            Kirja.PrintData();
+
+        }
+    }
+}
