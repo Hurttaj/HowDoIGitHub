4ba7dcc372d15de50f61c5d4be46bd58db5233f0
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-20 14:45:07 +0200

    Copypasta on vaarallista.

---
+++ Kotitehtävä/Radio/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio radio = new Radio();
+            radio.OnOff();
+            radio.Volume = 4;
+            radio.Frequency = 56000.0;
+            radio.Frequency = 2400.0;
+            radio.PrintData();
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Kotitehtävä/Radio/Radio.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio
+{
+    class Radio
+    {
+        public bool IsOn { get; set; }
+        private int volume;
+        private double frequency;
+
+        private readonly int maxVolume = 9;
+        private readonly int minVolume = 0;
+        private readonly double maxFrequency = 26000.0;
+        private readonly double minFrequency = 2000.0;
+
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value <= maxVolume && value >= minVolume)
+                {
+                    volume = value;
+                }
+                else
+                {
+                    Console.WriteLine("Volume out of bounds - no change.");
+                }
+            }
+        }
+
+        public double Frequency
+        {
+            get
+            {
+                return frequency;
+            }
+            set
+            {
+                if (value <= maxFrequency && value >= minFrequency)
+                {
+                    frequency = value;
+                }
+                else
+                {
+                    Console.WriteLine("Frequency out of bounds - no change.");
+                }
+            }
+        }
+
+        public void OnOff()
+        {
+            if (IsOn == true)
+            {
+                IsOn = false;
+            }
+            else
+            {
+                IsOn = true;
+            }
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Radio:");
+            Console.WriteLine("Is on: " + IsOn);
+            Console.WriteLine("Volume: {0} Frequency {1}", Volume, Frequency);
+        }
+
+    }
+}
