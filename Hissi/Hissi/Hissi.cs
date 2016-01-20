82f0e24f3526b90412bee06b173f58e254a24901
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-20 15:47:42 +0200

    Totuusarvoja

---
+++ Hissi/Hissi/Hissi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hissi
+{
+    class Hissi
+    {
+        private int floor;
+
+        private readonly int maxFloor = 5;
+        private readonly int minFloor = 1;
+
+        public int Floor
+        {
+            get
+            {
+                return floor;
+
+            }
+            set
+            {
+                if (value <= maxFloor && value >= minFloor)
+                {
+                    floor = value;
+                    Console.WriteLine("Hissi on kerroksessa " + value);
+                }
+                else
+                {
+                    Console.WriteLine("Näätkö jossain " + value + " napin. On kyllä aika velho kun menee olemattomiin kerroksiin.");
+                }
+            }
+        }
+        
+        }
+}
---
+++ Hissi/Hissi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hissi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hissi hissi = new Hissi();
+            {
+                bool x = true;
+                int y;
+                while (x == true)
+                {
+                    Console.WriteLine("Anna kerros: ");
+                    string line = Console.ReadLine();
+                    x = int.TryParse(line, out y);
+                    if (x == true)
+                    {
+                        hissi.Floor = y;
+                    }
+                    if (x == false)
+                    {
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
