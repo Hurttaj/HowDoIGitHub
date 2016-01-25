99adf2b296d0efcfbdf4729c6388fbd8c20ba5de
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-25 23:06:11 +0200

    Tehtävä6

---
+++ Kotitehtävä/Jotain/Kirja.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jotain
+{
+    class Kirja : Esine
+    {
+        public int PageCount { get; set; }
+        public string Author { get; set; }
+        public string Name { get; set; }
+
+        public Kirja(double weight, double value, int acq, int pagecount)
+            : base(weight,value,acq)
+        {
+            PageCount = pagecount;
+        }
+
+         public void PrintKirja()
+        {
+            Console.WriteLine("Kirja:");
+            Console.WriteLine("Weight: {0}, Value: {1}, Time of Acquisition: {2}, Page Count: {3}, Author: {4}", Weight, Value, Acq, PageCount, Author);
+        }
+    }
+}
---
+++ Kotitehtävä/Jotain/Laite.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jotain
+{
+    class Laite : Esine
+    {
+        public string OS { get; set; }
+
+        public Laite(double weight, double value, int acq)
+            : base(weight,value,acq)
+            {
+
+            }
+
+        public void PrintLaite()
+        {
+            Console.WriteLine("Laite:");
+            Console.WriteLine("Weight: {0}, Value: {1}, Time of Acquisition: {2}, OS: {3}", Weight, Value, Acq, OS);
+        }
+    }
+}
