5e4c62bed04f4d789a748a85f95b5753917072bf
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-26 22:41:13 +0200

    Ei helvetti

---
+++ ClassDiagram/ClassDiagram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDiagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Proletariat> proles = new List<Proletariat>();
+            Proletariat prole = new Proletariat();
+            prole.FirstName = "Ernst";
+            prole.LastName = "Busch";
+            Console.WriteLine(prole.Agitate());
+            prole.IsRevolutionary = true;
+        }
+    }
+}
