2c8b4c28baa081890ce45bb844efe1e6c40e8747
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-26 10:51:13 +0200

    Tyred

---
+++ Tyring/Tyring/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tyre nokia = new Tyre { Name = "Blorp", Model = "Erhmergerd", Size = "Derp16"};
+
+            Vehicle car = new Vehicle();
+            car.AddTyre(nokia);
+            car.AddTyre(nokia);
+            car.AddTyre(nokia);
+            car.AddTyre(nokia);
+            car.Name = "PoS";
+            car.Model = "Trabant";
+
+            Console.WriteLine(car.ToString());
+
+        }
+    }
+}
