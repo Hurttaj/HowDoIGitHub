f6064ebc0cd3bfd95a002b912aa66ebb8aae1c4b
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-27 14:37:07 +0200

    ghjg

--- Rekisteri/Rekisteri/Persons.cs
+++ Rekisteri/Rekisteri/Persons.cs
@@ -7,10 +7,71 @@ using System.Threading.Tasks;
 namespace Rekisteri
 {
     /// <summary>
-    /// Persons collection stores person objects.
+    /// This clsas holds persons and gives a few methods...
     /// </summary>
     class Persons
     {
+        /// <summary>
+        /// persons holds person objects...
+        /// </summary>
+        private List<Person> persons;
+
+        /// <summary>
+        /// Default constructor initializes a collection
+        /// </summary>
+        public Persons()
+        {
+            persons = new List<Person>();
+        }
+
+        /// <summary>
+        /// Adds a person to collection
+        /// </summary>
+        /// <param name="person"></param>
+        public void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+        /// <summary>
+        /// Returns a person to caller application.
+        /// </summary>
+        /// <param name="index">Person index in collection</param>
+        /// <returns>Returned person object</returns>
+        public Person GetPerson(int index)
+        {
+            if (index < persons.Count)
+            {
+                return persons.ElementAt(index);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Person FindPerson(string sSN)
+        {
+            foreach(Person person in persons)
+            {
+                if (sSN.Equals(person.SSN))
+                {
+                    //person with sotu found
+                    return person;
+                }
+               
+            }
+            return null;
+        }
+
+        public void PrintCollection()
+        {
+            Console.WriteLine("MRGLRGLRLGr");
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+
 
     }
 }
--- Rekisteri/Rekisteri/Program.cs
+++ Rekisteri/Rekisteri/Program.cs
@@ -10,6 +10,45 @@ namespace Rekisteri
     {
         static void Main(string[] args)
         {
+            // create an object from Persons
+            Persons myFriends = new Persons();
+
+            // create a friends
+            Person person1 = new Person { FirstName = "Jussi", LastName = "Jurkka", SSN = "101010-1337" };
+            Person person2 = new Person { FirstName = "Etunimi", LastName = "Sukunimi", SSN = "123456789" };
+            Person person3 = new Person { FirstName = "Omaperäisyys", LastName = "Kuolee", SSN = "6543210" };
+
+            // add persons to collection
+            myFriends.AddPerson(person1);
+            myFriends.AddPerson(person2);
+            myFriends.AddPerson(person3);
+
+            // print collection
+            myFriends.PrintCollection();
+
+            Console.WriteLine("Get one person from collection:");
+            Person person4 = myFriends.GetPerson(2);
+            if (person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Cannot find person in register.");
+            }
+
+            //find person with SSN
+            string sSN = "101010-1337";
+            Console.WriteLine("Find person with SSN: " + sSN);
+            Person person5 = myFriends.FindPerson(sSN);
+            if (person5 != null)
+            {
+                Console.WriteLine(person5.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Not found.");
+            }
         }
     }
 }
