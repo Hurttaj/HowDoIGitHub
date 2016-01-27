e2906e5e2b183d6f57e2a9de06d11cee2fb92b95
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-27 14:51:15 +0200

    dsfrger

---
+++ CD/CD/CD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD
+{
+    class CD
+    {
+        public string Artist { get; set; }
+        public string Name { get; set; }
+
+        public List<Song> songs;
+    }
+}
