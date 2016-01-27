113635972afdadce00e7a4fabea5ecf997049373
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-27 13:46:34 +0200

    hgfgdfyhefd

---
+++ Rekisteri/Rekisteri/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rekisteri
+{
+    /// <summary>
+    /// This class contains Person properties
+    /// </summary>
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SSN { get; set; }
+
+        public override string ToString()
+        {
+            return "First name: " + FirstName + "Last name: " + LastName + "Social Security Number: " + SSN;
+        }
+    }
+}
